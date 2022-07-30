30653518ab505ac0a7b91fc8fda66decff4317e3
Raj Yadav <vector36razz@gmail.com>
2022-07-30 15:05:34 +0545

    Arrays file
    Arrays and ArrayList file added

---
+++ Arrays_ArrayList/Basic.java
@@ -0,0 +1,20 @@
+package com.rk.Java_Basics.Arrays_ArrayList;
+
+public class Basic {
+    public static void main(String[] args) {
+        //Syntax
+        /*
+        datatype[] variable_name = new datatype[size];
+         */
+
+        //e.g: int[] arr = new int[5];
+        /*
+        here, int[] arr - declaration 0f arrays(initialization)
+        -new int[5] = actual memory allocation in heap memory
+        -new = used to create new object in heap memory
+         */
+ //other way to declare array
+  int[] arr = {1,2,3,4};
+  //int[] arr is compile time
+    }
+}
